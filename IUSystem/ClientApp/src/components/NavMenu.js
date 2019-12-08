86872757efeae348e94a22f05c2707006f3a536e
Yordan <pm.yordan@gmail.com>
2019-12-08 12:43:08 +0200

    Changed style.

--- IUSystem/ClientApp/src/components/NavMenu.js
+++ IUSystem/ClientApp/src/components/NavMenu.js
@@ -25,21 +25,21 @@ export class NavMenu extends Component {
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Navbar color="dark" className="navbar-expand-sm navbar-toggleable-sm ng-black box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">IUSystem</NavbarBrand>
+            <NavbarBrand tag={Link} className="text-white" to="/">IUSystem</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-white" to="/">Home</NavLink>
                 </NavItem>
-                <NavItem>
+                {/* <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
+                </NavItem> */}
                 <LoginMenu>
                 </LoginMenu>
               </ul>
--- IUSystem/ClientApp/src/components/api-authorization/Login.js
+++ IUSystem/ClientApp/src/components/api-authorization/Login.js
@@ -51,9 +51,9 @@ export class Login extends Component {
         } else {
             switch (action) {
                 case LoginActions.Login:
-                    return (<div>Processing login</div>);
+                    return (<div style={{color: "white"}}>Processing login</div>);
                 case LoginActions.LoginCallback:
-                    return (<div>Processing login callback</div>);
+                    return (<div style={{color: "red"}}>Processing login callback</div>);
                 case LoginActions.Profile:
                 case LoginActions.Register:
                     return (<div></div>);
